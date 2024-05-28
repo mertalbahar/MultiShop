5e50aa54013eb1a57d6f550ef4d2951bca0fab4d
MertalBahar <mertalbahar@windowslive.com>
2024-05-28 19:59:41 +0300

    Order Microservice created. Onion Arch, CQRS, Mediator, Docker, Portainer used.

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderDetailsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+
+namespace MultiShop.Order.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderDetailsController : ControllerBase
+{
+    private readonly GetOrderDetailQueryHandler _getOrderDetailQueryHandler;
+    private readonly GetOrderDetailByIdQueryHandler _getOrderDetailByIdQueryHandler;
+    private readonly CreateOrderDetailCommandHandler _createOrderDetailCommandHandler;
+    private readonly UpdateOrderDetailCommandHandler _updateOrderDetailCommandHandler;
+    private readonly RemoveOrderDetailCommandHandler _removeOrderDetailCommandHandler;
+
+    public OrderDetailsController(GetOrderDetailQueryHandler getOrderDetailQueryHandler,
+        GetOrderDetailByIdQueryHandler getOrderDetailByIdQueryHandler,
+        CreateOrderDetailCommandHandler createOrderDetailCommandHandler,
+        UpdateOrderDetailCommandHandler updateOrderDetailCommandHandler,
+        RemoveOrderDetailCommandHandler removeOrderDetailCommandHandler)
+    {
+        _getOrderDetailQueryHandler = getOrderDetailQueryHandler;
+        _getOrderDetailByIdQueryHandler = getOrderDetailByIdQueryHandler;
+        _createOrderDetailCommandHandler = createOrderDetailCommandHandler;
+        _updateOrderDetailCommandHandler = updateOrderDetailCommandHandler;
+        _removeOrderDetailCommandHandler = removeOrderDetailCommandHandler;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> OrderDetailList()
+    {
+        List<GetOrderDetailQueryResult> values = await _getOrderDetailQueryHandler.Handle();
+
+        return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetOrderDetailById(int id)
+    {
+        GetOrderDetailByIdQueryResult value = await _getOrderDetailByIdQueryHandler.Handle(new GetOrderDetailByIdQuery(id));
+
+        return Ok(value);
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateOrderDetail(CreateOrderDetailCommand command)
+    {
+        await _createOrderDetailCommandHandler.Handle(command);
+
+        return Ok("Sipari detayı başarıyla eklendi.");
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateOrderDetail(UpdateOrderDetailCommand command)
+    {
+        await _updateOrderDetailCommandHandler.Handle(command);
+
+        return Ok("Sipari detayı başarıyla güncellendi.");
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteOrderDetail(int id)
+    {
+        await _removeOrderDetailCommandHandler.Handle(new RemoveOrderDetailCommand(id));
+
+        return Ok("Sipari detayı başarıyla silindi.");
+    }
+}
---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            List<GetOrderingQueryResult> values = await _mediator.Send(new GetOrderingQuery());
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderingById(int id)
+        {
+            GetOrderingByIdQueryResult value = await _mediator.Send(new GetOrderingByIdQuery(id));
+
+            return Ok(value);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateOrdering(CreateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok("Sipariş başarıyla eklendi.");
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok("Sipariş başarıyla güncellendi.");
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteOrdering(int id)
+        {
+            await _mediator.Send(new RemoveOrderingCommand(id));
+
+            return Ok("Sipariş başarıyla silindi.");
+        }
+    }
+}
