fabfa460bfb983404d9f461a5c98dfc27ad1b32e
MertalBahar <mertalbahar@windowslive.com>
2024-07-04 14:18:24 +0300

    ShoppingCart layout and partial views created. Contact page created with static razor pages.

---
+++ Frontends/MultiShop.WebUI/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MultiShop.WebUI.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
