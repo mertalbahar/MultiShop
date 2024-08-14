5f57f1fdd7381f45e90e7b13d34b4967592fe9aa
MertalBahar <mertalbahar@windowslive.com>
2024-08-14 11:37:57 +0300

    User registration created. 1. UserRegisterDto created. 2. UserController with CreateUser method created. 3. CreateUser page created.

---
+++ Frontends/MultiShop.WebUI/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.IdentityDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Controllers
+{
+	public class UserController : Controller
+	{
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateUser(UserRegisterDto userRegisterDto)
+        {
+            if (userRegisterDto.Password.Equals(userRegisterDto.ConfirmPassword))
+            {
+                HttpClient client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(userRegisterDto);
+                StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("https://localhost:5001/api/Registers", stringContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+                return View();
+        }
+	}
+}
