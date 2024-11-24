f042e4145220853db9df1304123bc81bc2478e06
MertalBahar <mertalbahar@windowslive.com>
2024-11-24 09:38:17 +0300

    Messages for Admin area completed. Frontend: 1. MessageController in admin area created. 2. Inbox, InboxMessageDetail, Sendbox and SendboxMessageDetail pages created. 3. _AdminSidebar updated. 4. _ViewImports updated for IdentityDto.

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.IdentityDtos;
+using MultiShop.DtoLayer.MessageDtos;
+using MultiShop.WebUI.Services.Abstracts;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MessageController : Controller
+    {
+        private readonly IServiceManager _manager;
+        private readonly IMapper _mapper;
+
+        public MessageController(IServiceManager manager, IMapper mapper)
+        {
+            _manager = manager;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            UserDetailDto user = await _manager.UserService.GetUserDetailAsync();
+            List<ResultInboxUserMessageDto> values = await _manager.MessageService.GetAllInboxMessagesAsync(user.Id);
+
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> InboxMessageDetail([FromRoute(Name = "id")] int id)
+        {
+            GetByIdUserMessageDto values = await _manager.MessageService.GetByIdUserMessageAsync(id);
+            UpdateUserMessageDto result = _mapper.Map<UpdateUserMessageDto>(values);
+
+            return View(result);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> InboxMessageDetail([FromForm] UpdateUserMessageDto updateUserMessageDto)
+        {
+            await _manager.MessageService.UpdateUserMessageAsync(updateUserMessageDto);
+
+            return RedirectToAction("Inbox", "Message", new { area = "Admin" });
+        }
+
+        public async Task<IActionResult> Sendbox()
+        {
+            UserDetailDto user = await _manager.UserService.GetUserDetailAsync();
+            List<ResultSendboxUserMessageDto> values = await _manager.MessageService.GetAllSendboxMessagesAsync(user.Id);
+
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendboxMessageDetail([FromRoute(Name = "id")] int id)
+        {
+            GetByIdUserMessageDto values = await _manager.MessageService.GetByIdUserMessageAsync(id);
+
+            return View(values);
+        }
+    }
+}
