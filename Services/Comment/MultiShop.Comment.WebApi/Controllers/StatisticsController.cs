7ae81a7178ed48bf8c8a1069c45814fd489e5f97
MertalBahar <mertalbahar@windowslive.com>
2024-12-10 12:16:22 +0300

    Comment StatisticService created. Comment Microservice: 1. IStatisticService and StatisticService created. 2. GetActiveCommentCount, GetPassiveCommentCount and GetTotalCommentCount methods created. 3. StatisticsController created. 4. ServiceRegistrationExtension updated for StatisticService. 5. IServiceManager and ServiceManager updated for StatisticService.

---
+++ Services/Comment/MultiShop.Comment.WebApi/Controllers/StatisticsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.WebApi.Services.Abstracts;
+
+namespace MultiShop.Comment.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public StatisticsController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet("getActiveCommentCount")]
+        public IActionResult GetActiveCommentCount()
+        {
+            int value = _manager.StatisticService.GetActiveCommentCount();
+
+            return Ok(value);
+        }
+        
+        [HttpGet("getPassiveCommentCount")]
+        public IActionResult GetPassiveCommentCount()
+        {
+            int value = _manager.StatisticService.GetPassiveCommentCount();
+
+            return Ok(value);
+        }
+
+        [HttpGet("getTotalCommentCount")]
+        public IActionResult GetTotalCommentCount()
+        {
+            int value = _manager.StatisticService.GetTotalCommentCount();
+
+            return Ok(value);
+        }
+    }
+}
