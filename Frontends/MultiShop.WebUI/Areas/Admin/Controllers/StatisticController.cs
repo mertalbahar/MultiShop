a2841a339c88b547dfb3a5e94cd87b7ffc0defa8
MertalBahar <mertalbahar@windowslive.com>
2024-12-10 20:55:36 +0300

    Catalog statistics created in frontend. Frontend: 1. ICatalogStatisticService and CatalogStatisticService created. 2. IServiceManager and ServiceManager updated for CatalogStatisticService. 3. HttpClientRegistrationExtension updated for CatalogStatisticService. 4. StatisticController and Index page created in admin area. 5. CatalogStatisticViewModel created in admin area. 6. StatisticViewModel created in admin area. 7. _ViewImports updated for view models.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/StatisticController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -1,13 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Areas.Admin.Models;
+using MultiShop.WebUI.Services.Abstracts;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class StatisticController : Controller
     {
-        public IActionResult Index()
+        private readonly IServiceManager _manager;
+
+        public StatisticController(IServiceManager manager)
         {
-            return View();
+            _manager = manager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var brandCount = await _manager.CatalogStatisticServices.GetBrandCount();
+            var categoryCount = await _manager.CatalogStatisticServices.GetCategoryCount();
+            var productCount = await _manager.CatalogStatisticServices.GetProductCount();
+            var productAvgPrice = await _manager.CatalogStatisticServices.GetProductAvgPrice();
+            var maxPriceProductName = await _manager.CatalogStatisticServices.GetMaxPriceProductName();
+            var minPriceProductName = await _manager.CatalogStatisticServices.GetMinPriceProductName();
+
+            var catalogStatistics = new CatalogStatisticViewModel
+            {
+                BrandCount = brandCount,
+                CategoryCount = categoryCount,
+                ProductCount = productCount,
+                ProductAvgPrice = productAvgPrice,
+                MaxPriceProductName = maxPriceProductName,
+                MinPriceProductName = minPriceProductName
+            };
+
+            var statisticViewModel = new StatisticViewModel
+            {
+                CatalogStatistics = catalogStatistics
+            };
+
+            return View(statisticViewModel);
         }
     }
 }
